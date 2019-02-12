9899d2f9e499f164a01dbc36f2467da7114b3c3c
Daniel <44640447+Semibot@users.noreply.github.com>
2019-02-12 14:56:44 +0100

    Student/teacher class updated

--- AttendanceAutomation/src/attendanceautomation/be/Student.java
+++ AttendanceAutomation/src/attendanceautomation/be/Student.java
@@ -1,42 +1,44 @@
 package attendanceautomation.be;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author DKE
  */
 public class Student{
     private int id;
-    private String firstName;
-    private String lastName;
+    private String name;
+    private LocalDate date;
     
-    public Student(int id, String firstName, String lastName){
+    public Student(int id, String name, LocalDate date){
         this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.name = name;
+        this.date = date;
     }
     
     @Override
     public String toString(){
-        return firstName + " " + lastName;
+        return name + date;
     }
     
     public int getId(){
         return id;
     }
     
-    public String getFirstName(){
-        return firstName;
+    public String getName(){
+        return name;
     }
     
-    public void setFirstName(String firstName){
-        this.firstName = firstName;
+    public void setName(String name){
+        this.name = name;
     }
     
-    public String getLastName(){
-        return lastName;
+    public LocalDate getDate(){
+        return date;
     }
     
-    public void setLastName(String lastName){
-        this.lastName = lastName;
+    public void setDate(LocalDate date){
+        this.date = date;
     }
 }
--- AttendanceAutomation/src/attendanceautomation/be/Teacher.java
+++ AttendanceAutomation/src/attendanceautomation/be/Teacher.java
@@ -1,32 +1,34 @@
 package attendanceautomation.be;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author DKE
  */
 public class Teacher{
     private int id;
-    private String firstName;
-    private String lastName;
+    private String name;
+    private LocalDate date;
     
     @Override
     public String toString(){
-        return firstName + lastName;
+        return name + date;
     }
     
-    public String getFirstName(){
-        return firstName;
+    public String getName(){
+        return name;
     }
-
-    public void setFirstName(String firstName){
-        this.firstName = firstName;
+    
+    public void setName(String name){
+        this.name = name;
     }
-
-    public String getLastName(){
-        return lastName;
+    
+    public LocalDate getDate(){
+        return date;
     }
-
-    public void setLastName(String lastName){
-        this.lastName = lastName;
+    
+    public void setDate(LocalDate date){
+        this.date = date;
     }
 }

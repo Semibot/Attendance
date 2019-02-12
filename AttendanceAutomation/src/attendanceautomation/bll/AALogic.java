1a44020daef5d53f4dbc1985059e4099f64aa4ec
Daniel <44640447+Semibot@users.noreply.github.com>
2019-02-12 15:54:24 +0100

    bll logic layer implemented

---
+++ AttendanceAutomation/src/attendanceautomation/bll/AALogic.java
@@ -0,0 +1,37 @@
+package attendanceautomation.bll;
+
+import attendanceautomation.be.Student;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author DKE
+ */
+public class AALogic{
+    
+    public Student createStudent(Student s) throws SQLException{
+        StudentDAO sdao = new StudentDAO();
+        return sdao.createStudent(0, s);
+    }
+    
+    public Student getStudent(int id){
+        StudentDAO sdao = new StudentDAO();
+        return sdao.getStudent(id);
+    }
+    
+    public void updateStudent(Student s) throws SQLException{
+        StudentDAO sdao = new StudentDAO();
+        sdao.updateStudent(s);
+    }
+    
+    public void deleteStudent(Student s) throws SQLException{
+        StudentDAO sdao = new StudentDAO();
+        sdao.deleteStudent(s);
+    }
+    
+    public List<Student> getAllStudents(){
+        StudentDAO sdao = new StudentDAO();
+        return sdao.getAllStudents();
+    }
+}

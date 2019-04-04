04e8f8a83a0b73d0cb531356a493237963f7e415
Daniel <44640447+Semibot@users.noreply.github.com>
2019-04-04 11:22:31 +0200

    Be.present class and createComboBoxItems method created

--- AttendanceAutomation/src/attendanceautomation/gui/AttendanceController.java
+++ AttendanceAutomation/src/attendanceautomation/gui/AttendanceController.java
@@ -1,11 +1,15 @@
 package attendanceautomation.gui;
 
+import attendanceautomation.be.Student;
+import attendanceautomation.bll.AALogic;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXPasswordField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,11 +21,24 @@ import javafx.scene.control.Label;
  */
 public class AttendanceController implements Initializable{
     
-    @FXML private JFXComboBox nameComboBox;
+    @FXML private JFXComboBox<Student> nameComboBox;
     @FXML private JFXDatePicker datepicker;
     @FXML private JFXPasswordField passwordField;
     @FXML private JFXButton submitBtn;
     @FXML private Label submitLabel;
+    private AALogic bll;
+    
+    public AttendanceController(){
+        bll = new AALogic();
+    }
+    
+    private void createComboBoxItems(){
+        ObservableList<Student> studentComboBoxList =
+                FXCollections.observableArrayList(
+                bll.getAllStudents());
+        nameComboBox.getItems().addAll(studentComboBoxList);
+        nameComboBox.setPromptText("Enter name");
+    }
     
     @FXML
     private void submitBtnAction(ActionEvent e){
@@ -36,7 +53,7 @@ public class AttendanceController implements Initializable{
     
     @Override
     public void initialize(URL url, ResourceBundle rb){
-        nameComboBox.setPromptText("Enter name");
+        createComboBoxItems();
         datepicker.setPromptText("Select a date");
         passwordField.setPromptText("Enter paassword");
     }

7abee6fc43524df07b545d84d98cc86912e3d446
Daniel <44640447+Semibot@users.noreply.github.com>
2020-04-14 21:09:12 +0200

    fx:id's moved to controller

--- AttendanceAutomation/src/attendanceautomation/gui/model/AAModel.java
+++ AttendanceAutomation/src/attendanceautomation/gui/model/AAModel.java
@@ -7,24 +7,15 @@ import attendanceautomation.bll.AAStudentLogic;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  *
  * @author Daniel
  */
 public class AAModel{
-    private final ObservableList data = getPresenceData();
-    @FXML private TableView<Presence> attendanceListTableView = new TableView();
-    @FXML private TableColumn<Presence, LocalDate> dateColumn = new TableColumn();
-    @FXML private TableColumn<Presence, String> attendanceColumn = new TableColumn();
     private AAPresenceLogic aapl;
     private AAStudentLogic aasl;
     
@@ -33,19 +24,12 @@ public class AAModel{
         aasl = new AAStudentLogic();
     }
     
-    public void populateTableView(){
-        dateColumn.setCellValueFactory(new PropertyValueFactory<>("currentDate"));
-        attendanceColumn.setCellValueFactory(new PropertyValueFactory<>("isPresent"));
-        attendanceListTableView.setItems(data);
-    }
-    
-    private ObservableList getPresenceData(){
-        List list = new ArrayList();
-        list.add(new Presence(LocalDate.of(1980, Month.APRIL, 6), "Present"));
-        list.add(new Presence(LocalDate.of(1992, Month.APRIL, 5), "Absent"));
-        
-        ObservableList data = FXCollections.observableArrayList(list);
-        return data;
+    public ObservableList<Presence> getPresenceData(){
+        ObservableList<Presence> presence = FXCollections.observableArrayList();
+        presence.add(new Presence(LocalDate.of(1980, Month.APRIL, 6), "Present"));
+        presence.add(new Presence(LocalDate.of(1992, Month.APRIL, 5), "Absent"));
+        presence.add(new Presence(LocalDate.of(1988, Month.FEBRUARY, 16), "Absent"));
+        return presence;
     }
     
     public Presence createPresence(Presence p) throws SQLException{

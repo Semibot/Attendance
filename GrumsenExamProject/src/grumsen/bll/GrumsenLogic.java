588453369a48b62959599add5c47043ec8fa5096
Daniel <44640447+Semibot@users.noreply.github.com>
2020-04-24 00:18:23 +0200

    createAdmin simple pass through

--- GrumsenExamProject/src/grumsen/bll/GrumsenLogic.java
+++ GrumsenExamProject/src/grumsen/bll/GrumsenLogic.java
@@ -1,6 +1,8 @@
 package grumsen.bll;
 
+import grumsen.be.Admin;
 import grumsen.be.User;
+import grumsen.dal.AdminDAO;
 import grumsen.dal.UserDAO;
 import java.sql.SQLException;
 
@@ -13,5 +15,10 @@ public class GrumsenLogic{
     public User createUser(User u) throws SQLException{
         UserDAO udao = new UserDAO();
         return udao.createUser(0, u);
+    }
+    
+    public Admin createAdmin(Admin a) throws SQLException{
+        AdminDAO adao = new AdminDAO();
+        return adao.createAdmin(0, a);
     }
 }

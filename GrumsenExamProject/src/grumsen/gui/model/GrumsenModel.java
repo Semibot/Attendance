63ba4440e2fad4d73468191e44f1447caafdabaf
Daniel <44640447+Semibot@users.noreply.github.com>
2020-04-24 00:22:51 +0200

    createAdmin simple pass through

--- GrumsenExamProject/src/grumsen/gui/model/GrumsenModel.java
+++ GrumsenExamProject/src/grumsen/gui/model/GrumsenModel.java
@@ -1,5 +1,6 @@
 package grumsen.gui.model;
 
+import grumsen.be.Admin;
 import grumsen.be.User;
 import grumsen.bll.GrumsenLogic;
 import java.sql.SQLException;
@@ -13,5 +14,10 @@ public class GrumsenModel{
     public User createUser(User u) throws SQLException{
         GrumsenLogic usergl = new GrumsenLogic();
         return usergl.createUser(u);
+    }
+    
+    public Admin createAdmin(Admin a) throws SQLException{
+        GrumsenLogic admingl = new GrumsenLogic();
+        return admingl.createAdmin(a);
     }
 }

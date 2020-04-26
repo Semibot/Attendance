caa2999cf063dfbe844282123e4dcbf9a40d905a
Daniel <44640447+Semibot@users.noreply.github.com>
2020-04-26 17:30:38 +0200

    customer created

---
+++ GrumsenExamProject/src/grumsen/be/Customer.java
@@ -0,0 +1,52 @@
+package grumsen.be;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Customer{
+    private final IntegerProperty id = new SimpleIntegerProperty();
+    private final StringProperty name = new SimpleStringProperty();
+    private final IntegerProperty projectId = new SimpleIntegerProperty();
+    
+    public int getProjectId(){
+        return projectId.get();
+    }
+    
+    public void setProjectId(int value){
+        projectId.set(value);
+    }
+    
+    public IntegerProperty projectIdProperty(){
+        return projectId;
+    }
+    
+    public String getName(){
+        return name.get();
+    }
+    
+    public void setName(String value){
+        name.set(value);
+    }
+    
+    public StringProperty nameProperty(){
+        return name;
+    }
+    
+    public int getId(){
+        return id.get();
+    }
+    
+    public void setId(int value){
+        id.set(value);
+    }
+    
+    public IntegerProperty idProperty(){
+        return id;
+    }
+}

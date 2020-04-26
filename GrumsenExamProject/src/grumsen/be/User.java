d5474a91796e3ac7f546baa822a63df54cdeb7f4
Daniel <44640447+Semibot@users.noreply.github.com>
2020-04-26 17:31:23 +0200

    user edited

--- GrumsenExamProject/src/grumsen/be/User.java
+++ GrumsenExamProject/src/grumsen/be/User.java
@@ -2,17 +2,18 @@ package grumsen.be;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 
 /**
  *
  * @author Daniel
  */
-public class User{
-    private final IntegerProperty id = new SimpleIntegerProperty();
-    private final StringProperty name = new SimpleStringProperty();
-    private final IntegerProperty projectId = new SimpleIntegerProperty();
+public class User extends Person{
+    private final IntegerProperty projectId;
+    
+    public User(int id, String name, int projectId){
+        super(id, name);
+        this.projectId = new SimpleIntegerProperty(projectId);
+    }
     
     public int getProjectId(){
         return projectId.get();
@@ -24,29 +25,5 @@ public class User{
     
     public IntegerProperty projectIdProperty(){
         return projectId;
-    }
-    
-    public String getName(){
-        return name.get();
-    }
-    
-    public void setName(String value){
-        name.set(value);
-    }
-    
-    public StringProperty nameProperty(){
-        return name;
-    }
-    
-    public int getId(){
-        return id.get();
-    }
-    
-    public void setId(int value){
-        id.set(value);
-    }
-    
-    public IntegerProperty idProperty(){
-        return id;
     }
 }

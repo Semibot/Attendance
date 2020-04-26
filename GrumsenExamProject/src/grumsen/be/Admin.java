93abf63c3ba8fb461971a6b97f8b9f2558cf0beb
Daniel <44640447+Semibot@users.noreply.github.com>
2020-04-26 17:29:32 +0200

    admin updated

--- GrumsenExamProject/src/grumsen/be/Admin.java
+++ GrumsenExamProject/src/grumsen/be/Admin.java
@@ -2,17 +2,18 @@ package grumsen.be;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 
 /**
  *
  * @author Daniel
  */
-public class Admin{
-    private final IntegerProperty id = new SimpleIntegerProperty();
-    private final StringProperty name = new SimpleStringProperty();
-    private final IntegerProperty projectId = new SimpleIntegerProperty();
+public class Admin extends Person{
+    private final IntegerProperty projectId;
+    
+    public Admin(int id, String name, int projectId){
+        super(id, name);
+        this.projectId = new SimpleIntegerProperty(projectId);
+    }
     
     public int getProjectId(){
         return projectId.get();
@@ -24,29 +25,5 @@ public class Admin{
     
     public IntegerProperty projectIdProperty(){
         return projectId;
-    }
-    
-    public String getName(){
-        return name.get();
-    }
-    
-    public void setName(String value){
-        name.set(value);
-    }
-    
-    public StringProperty nameProperty(){
-        return name;
-    }
-    
-    public int getId(){
-        return id.get();
-    }
-    
-    public void setId(int value){
-        id.set(value);
-    }
-    
-    public IntegerProperty idProperty(){
-        return id;
     }
 }

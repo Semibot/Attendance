486a8b7b6f97be6d0478be35a58413744a4c0e1f
Daniel <44640447+Semibot@users.noreply.github.com>
2020-04-26 17:33:48 +0200

    PersonDAO created

---
+++ GrumsenExamProject/src/grumsen/dal/PersonDAO.java
@@ -0,0 +1,40 @@
+package grumsen.dal;
+
+import grumsen.be.Person;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Daniel
+ */
+public class PersonDAO{
+    DBConnector dbConnect = new DBConnector();
+    
+    public Person createPerson(int id, Person p) throws SQLException{
+        try(Connection conn = dbConnect.getConnection()){
+            String sql = "INSERT INTO Person(name) VALUES(?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, p.getName());
+            
+            int createdRows = pstmt.executeUpdate();
+            
+            if(createdRows == 0){
+                throw new SQLException("Creating a person failed, no rows created.");
+            }
+            
+            try(ResultSet generatedKeys = pstmt.getGeneratedKeys()){
+                if(generatedKeys.next()){
+                    p.setId((int)generatedKeys.getLong(1));
+                }else {
+                    throw new SQLException("Creating a person failed, no ID obtained");
+                }
+            }
+        }
+        return p;
+    }
+}

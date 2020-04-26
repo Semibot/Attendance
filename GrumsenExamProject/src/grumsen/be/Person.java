ab99b87cd1d230c26b40e010b807c5f375819491
Daniel <44640447+Semibot@users.noreply.github.com>
2020-04-26 17:30:14 +0200

    person superclass

---
+++ GrumsenExamProject/src/grumsen/be/Person.java
@@ -0,0 +1,44 @@
+package grumsen.be;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Person{
+    private final IntegerProperty id;
+    private final StringProperty name;
+    
+    public Person(int id, String name){
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+    }
+    
+    public String getName(){
+        return name.get();
+    }
+    
+    public void setName(String value){
+        name.set(value);
+    }
+    
+    public StringProperty nameProperty(){
+        return name;
+    }
+    
+    public int getId(){
+        return id.get();
+    }
+    
+    public void setId(int value){
+        id.set(value);
+    }
+    
+    public IntegerProperty idProperty(){
+        return id;
+    }
+}

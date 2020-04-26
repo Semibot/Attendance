31fcace10d66f416cf7dc25977f9e6d664260ad1
Daniel <44640447+Semibot@users.noreply.github.com>
2020-04-26 17:35:28 +0200

    Model edited

--- GrumsenExamProject/src/grumsen/gui/model/GrumsenModel.java
+++ GrumsenExamProject/src/grumsen/gui/model/GrumsenModel.java
@@ -1,6 +1,8 @@
 package grumsen.gui.model;
 
 import grumsen.be.Admin;
+import grumsen.be.Customer;
+import grumsen.be.Person;
 import grumsen.be.User;
 import grumsen.bll.GrumsenLogic;
 import java.sql.SQLException;
@@ -11,13 +13,23 @@ import java.sql.SQLException;
  */
 public class GrumsenModel{
     
-    public User createUser(User u) throws SQLException{
-        GrumsenLogic usergl = new GrumsenLogic();
-        return usergl.createUser(u);
+    public Person createPerson(Person p) throws SQLException{
+        GrumsenLogic persongl = new GrumsenLogic();
+        return persongl.createPerson(p);
     }
     
     public Admin createAdmin(Admin a) throws SQLException{
         GrumsenLogic admingl = new GrumsenLogic();
         return admingl.createAdmin(a);
+    }
+    
+    public Customer createCustomer(Customer c) throws SQLException{
+        GrumsenLogic customergl = new GrumsenLogic();
+        return customergl.createCustomer(c);
+    }
+    
+    public User createUser(User u) throws SQLException{
+        GrumsenLogic usergl = new GrumsenLogic();
+        return usergl.createUser(u);
     }
 }
